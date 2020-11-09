3ae45876e661242d8f600db19ce89f5355155380
Ubuntu <pasen001@ucr.edu>
2020-11-08 22:45:07 -0800

    finished all parts

---
+++ turnin/pasen001_lab7_part3.c
@@ -0,0 +1,42 @@
+/*	Author: lab
+ *  Partner(s) Name: Peter John Asence
+ *	Lab Section:
+ *	Assignment: Lab #7  Exercise #3
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ *
+ *	Demo Link: https://youtu.be/jY3QhXn2De8
+ */
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+void ADC_init(){
+	ADCSRA |= (1 << ADEN | 1 << ADSC | 1 << ADATE);
+}
+
+int main(void) {
+    /* Insert DDR and PORT initializations */
+	DDRB = 0xFF; PORTB = 0x00;
+	DDRD = 0xFF; PORTD = 0x00;
+	DDRA = 0x00; PORTA = 0xFF;
+    /* Insert your solution below */
+	ADC_init();
+       	unsigned short x;
+	unsigned char B_LEDS;
+	unsigned char D_LEDS;
+	unsigned char max = 195;// about 195 depends on the intensity of the light 
+    while (1) {
+	x = ADC; // short = 16 bits
+	//B_LEDS = (char)x;	// char = 8 bits
+	//D_LEDS =(char)(x >> 8);
+	//PORTB = B_LEDS;
+	//PORTD = D_LEDS;
+	if (x >= max/2) PINB = 0x01;
+	else if (x < max/2) PINB = 0x00;
+    }
+    return 1;
+}
---
+++ turnin/pasen001_lab7_part4.c
@@ -0,0 +1,50 @@
+/*	Author: lab
+ *  Partner(s) Name: Peter John Asence
+ *	Lab Section:
+ *	Assignment: Lab #7  Exercise #4
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ *
+ *	Demo Link: https://youtu.be/7-vapDounZE 
+ */
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+void ADC_init(){
+	ADCSRA |= (1 << ADEN | 1 << ADSC | 1 << ADATE);
+}
+
+int main(void) {
+    /* Insert DDR and PORT initializations */
+	DDRB = 0xFF; PORTB = 0x00;
+	DDRD = 0xFF; PORTD = 0x00;
+	DDRA = 0x00; PORTA = 0xFF;
+    /* Insert your solution below */
+	ADC_init();
+       	unsigned short x;
+	//unsigned char B_LEDS;
+	//unsigned char D_LEDS;
+	unsigned char max = 195; // about 195 depends on the intensity of the light	
+    while (1) {
+	x = ADC; // short = 16 bits
+	//B_LEDS = (char)x;	// char = 8 bits
+	//D_LEDS =(char)(x >> 8);
+	//PORTB = B_LEDS;
+	//PORTD = D_LEDS;
+	if( x == 0) PORTB = 0x00;
+	else if (x >= 194) PORTB = 0xFF;
+	else if (x >= 171) PORTB = 0x7F;
+	else if (x >= 147) PORTB = 0x3F;
+	else if (x >= 123) PORTB = 0x1F;
+	else if (x >= 99) PORTB = 0x0F;
+	else if (x >= 75) PORTB = 0x07;
+	else if (x >= 51) PORTB = 0x03;
+	else if (x >= 6) PORTB = 0x01;
+	
+    }
+    return 1;
+}
